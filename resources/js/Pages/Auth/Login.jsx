7ba8889cd51431b4b3d9948cbbbf4f626e03cd7a
shayandutta <shayandutta23@gmail.com>
2025-07-14 22:24:02 +0530

    verify-email fix

--- resources/js/Pages/Auth/Login.jsx
+++ resources/js/Pages/Auth/Login.jsx
@@ -22,6 +22,7 @@ export default function Login({ status, canResetPassword }) {
 
     const submit = (e) => {
         e.preventDefault();
+
         post(route("login"));
     };
 
@@ -59,6 +60,7 @@ export default function Login({ status, canResetPassword }) {
                     <TextInput
                         id="password"
                         type="password"
+                        id="password"
                         name="password"
                         value={data.password}
                         className="mt-1 block w-full"
@@ -84,25 +86,16 @@ export default function Login({ status, canResetPassword }) {
                     </label>
                 </div>
 
-                <div className="flex items-center justify-between mt-4">
+                <div className="flex items-center justify-end mt-4">
                     {canResetPassword && (
                         <Link
                             href={route("password.request")}
-                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100"
+                            className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                         >
                             Forgot your password?
                         </Link>
                     )}
 
-                    <Link
-                        href={route("register")}
-                        className="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 ml-2"
-                    >
-                        Register
-                    </Link>
-                </div>
-
-                <div className="flex justify-end mt-4">
                     <PrimaryButton className="ms-4" disabled={processing}>
                         Log in
                     </PrimaryButton>
